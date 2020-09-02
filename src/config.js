6902ee4217dcb367da5533fecfe43b66d9af1a64
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-09-03 00:37:43 +0300

    push to heroku

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 
 //to be modified if needed, check w/ Rafa
 
-export const API_URL =  process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+export const API_URL =  process.env.REACT_APP_API_URL
